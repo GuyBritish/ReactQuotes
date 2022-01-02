ff7401339632d372be105be5f17d5f936f4b4b2b
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-01 23:46:55 -0600

    Add wraper component

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,5 +1,11 @@
+import React from "react";
+
 const Quotes = () => {
-	return <h1>All Quotes</h1>;
+	return (
+		<React.Fragment>
+			<h1>All Quotes</h1>
+		</React.Fragment>
+	);
 };
 
 export default Quotes;
