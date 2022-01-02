735245961b4a5f5ded3f94b89e8ae3d66e59b8e1
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-02 00:28:57 -0600

    Add quote list

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+import QuoteList from "../components/quotes/QuoteList";
+
+const SAMPLE_QUOTES = [
+	{ id: "q1", author: "Max", text: "Learning React is fun" },
+	{ id: "q2", author: "Khai", text: "Learning React is fun" },
+];
+
 const Quotes = () => {
 	return (
 		<React.Fragment>
 			<h1>All Quotes</h1>
+			<QuoteList quotes={SAMPLE_QUOTES} />
 		</React.Fragment>
 	);
 };
