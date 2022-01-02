1f626b3bcfc81f2a6d7fb43a1367b9ecc4d77c8b
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-02 00:44:37 -0600

    Add redirection on form submission

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuotes = () => {
+	const history = useHistory();
 	const addQuoteHandler = (quote) => {
 		console.log(quote);
+
+		history.push("/quotes");
 	};
 	return (
 		<React.Fragment>
