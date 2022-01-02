3c2ac46db90d040dd24ae2c234b1eb5bc30a5777
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-02 00:04:32 -0600

    Add navbar

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import Quotes from "./pages/Quotes";
 import QuoteDetail from "./pages/QuoteDetails";
 import NewQuotes from "./pages/NewQuote";
 
+import Layout from "./components/layout/Layout";
+
 function App() {
 	return (
-		<Switch>
-			<Route path="/" exact>
-				<Redirect to="/quotes" />
-			</Route>
-			<Route path="/quotes" exact>
-				<Quotes />
-			</Route>
-			<Route path="/quotes/:id">
-				<QuoteDetail />
-			</Route>
-			<Route path="/newquotes">
-				<NewQuotes />
-			</Route>
-		</Switch>
+		<Layout>
+			<Switch>
+				<Route path="/" exact>
+					<Redirect to="/quotes" />
+				</Route>
+				<Route path="/quotes" exact>
+					<Quotes />
+				</Route>
+				<Route path="/quotes/:id">
+					<QuoteDetail />
+				</Route>
+				<Route path="/newquotes">
+					<NewQuotes />
+				</Route>
+			</Switch>
+		</Layout>
 	);
 }
 
