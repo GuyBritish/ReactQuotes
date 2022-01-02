2de889e06ecf74c1df0c35ca13637223c5c5f43f
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-02 00:36:51 -0600

    Show quote details

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -2,13 +2,28 @@ import React from "react";
 import { Route, useParams } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const SAMPLE_QUOTES = [
+	{ id: "q1", author: "Max", text: "Learning React is fun" },
+	{ id: "q2", author: "Khai", text: "Learning React is fun" },
+];
 
 const QuoteDetail = () => {
 	const params = useParams();
+
+	const quote = SAMPLE_QUOTES.find((quote) => {
+		return quote.id === params.id;
+	});
+
+	if (!quote) {
+		return <p>No quote found!</p>;
+	}
+
 	return (
 		<React.Fragment>
-			<h1>Quote Detail</h1>
-			<p>{params.id}</p>
+			<HighlightedQuote text={quote.text} author={quote.author} />
+
 			<Route path={`/quotes/${params.id}/comments`}>
 				<Comments />
 			</Route>
