0e14fd97933a7e8cf8194a154f744cd869c3861b
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-02 00:50:58 -0600

    Add warning message for form

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,47 +1,73 @@
-import { useRef } from 'react';
+import React, { useRef, useState } from "react";
 
-import Card from '../UI/Card';
-import LoadingSpinner from '../UI/LoadingSpinner';
-import classes from './QuoteForm.module.css';
+import { Prompt } from "react-router-dom";
+
+import Card from "../UI/Card";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import classes from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
-  const authorInputRef = useRef();
-  const textInputRef = useRef();
+	const [isEntering, setIsEntering] = useState(false);
 
-  function submitFormHandler(event) {
-    event.preventDefault();
+	const authorInputRef = useRef();
+	const textInputRef = useRef();
 
-    const enteredAuthor = authorInputRef.current.value;
-    const enteredText = textInputRef.current.value;
+	function submitFormHandler(event) {
+		event.preventDefault();
 
-    // optional: Could validate here
+		const enteredAuthor = authorInputRef.current.value;
+		const enteredText = textInputRef.current.value;
 
-    props.onAddQuote({ author: enteredAuthor, text: enteredText });
-  }
+		// optional: Could validate here
 
-  return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+		props.onAddQuote({ author: enteredAuthor, text: enteredText });
+	}
 
-        <div className={classes.control}>
-          <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
-        </div>
-      </form>
-    </Card>
-  );
+	const formFocusHandler = () => {
+		setIsEntering(true);
+	};
+
+	const finishEntering = () => {
+		setIsEntering(false);
+	};
+
+	return (
+		<React.Fragment>
+			<Prompt
+				when={isEntering}
+				message={(location) => {
+					return "Are you sure you want to leave? All of your progress will be lost.";
+				}}
+			/>
+			<Card>
+				<form
+					className={classes.form}
+					onSubmit={submitFormHandler}
+					onFocus={formFocusHandler}
+				>
+					{props.isLoading && (
+						<div className={classes.loading}>
+							<LoadingSpinner />
+						</div>
+					)}
+
+					<div className={classes.control}>
+						<label htmlFor="author">Author</label>
+						<input type="text" id="author" ref={authorInputRef} />
+					</div>
+					<div className={classes.control}>
+						<label htmlFor="text">Text</label>
+						<textarea id="text" rows="5" ref={textInputRef}></textarea>
+					</div>
+					<div className={classes.actions}>
+						<button onClick={finishEntering} className="btn">
+							Add Quote
+						</button>
+					</div>
+				</form>
+			</Card>
+		</React.Fragment>
+	);
 };
 
 export default QuoteForm;
