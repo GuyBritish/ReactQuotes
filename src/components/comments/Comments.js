3d80235c225ef38f11847cbb0adad9b37d0c2713
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-03 01:18:07 -0600

    Fetch comments

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,27 +1,65 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-import classes from './Comments.module.css';
-import NewCommentForm from './NewCommentForm';
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
+
+import NewCommentForm from "./NewCommentForm";
+import CommentList from "./CommentsList";
+
+import classes from "./Comments.module.css";
 
 const Comments = () => {
-  const [isAddingComment, setIsAddingComment] = useState(false);
+	const [isAddingComment, setIsAddingComment] = useState(false);
 
-  const startAddCommentHandler = () => {
-    setIsAddingComment(true);
-  };
-  
-  return (
-    <section className={classes.comments}>
-      <h2>User Comments</h2>
-      {!isAddingComment && (
-        <button className='btn' onClick={startAddCommentHandler}>
-          Add a Comment
-        </button>
-      )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
-    </section>
-  );
+	const params = useParams();
+	const id = params.id;
+
+	const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+	useEffect(() => {
+		sendRequest(id);
+	}, [sendRequest, id]);
+
+	const startAddCommentHandler = () => {
+		setIsAddingComment(true);
+	};
+
+	const addedCommentHandler = () => {};
+
+	let cmt;
+
+	if (status === "pending") {
+		cmt = (
+			<div className="centered">
+				<LoadingSpinner />
+			</div>
+		);
+	}
+
+	if (status === "completed" && loadedComments && loadedComments.length > 0) {
+		cmt = <CommentList comments={loadedComments} />;
+	}
+
+	if (status === "completed" && (!loadedComments || loadedComments.length === 0)) {
+		cmt = <p className="centered">No comments were added yet</p>;
+	}
+
+	return (
+		<section className={classes.comments}>
+			<h2>User Comments</h2>
+			{!isAddingComment && (
+				<button className="btn" onClick={startAddCommentHandler}>
+					Add a Comment
+				</button>
+			)}
+			{isAddingComment && (
+				<NewCommentForm id={params.id} onAddedComment={addedCommentHandler} />
+			)}
+			{cmt}
+		</section>
+	);
 };
 
 export default Comments;
